2ba6aa5e8b9df17ab7d6b0933c918be3f8a3aceb
Haruna Tijjani Zango <tjzango@gmail.com>
2022-02-02 00:46:54 +0100

    Created all system functionalities

--- product_barcode/settings.py
+++ product_barcode/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'index'
+    'widget_tweaks',
+    'index',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/auth/login/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/add/product'
